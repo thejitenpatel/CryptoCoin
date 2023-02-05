41040528d9973a595ee99954a8972d9d644c2d03
thejitenpatel <jiten.pateljp2001@gmail.com>
2023-02-05 22:07:32 +0530

    search bar functionality completed

--- CryptoCoin/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoCoin/Core/Home/ViewModels/HomeViewModel.swift
@@ -24,14 +24,33 @@ class HomeViewModel: ObservableObject{
     
     
     func addSubscriber() {
-        dataService.$allCoins
-            .sink{ [weak self] retunedCoins in
-                self?.allCoins = retunedCoins
+//        dataService.$allCoins
+//            .sink{ [weak self] retunedCoins in
+//                self?.allCoins = retunedCoins
+//            }
+//            .store(in: &cancellables)
+
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
-//            self.allCoins.append(DeveloperPreview.instance.coin)
-//            self.portfolioCoins.append(DeveloperPreview.instance.coin)
-//        }
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else{
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.lowercased().contains(lowercasedText)
+        }
     }
 }
