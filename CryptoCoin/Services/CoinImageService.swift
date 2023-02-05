4bd78570cd182418412aa06a7fc89d1000832a0f
thejitenpatel <jiten.pateljp2001@gmail.com>
2023-02-05 11:10:09 +0530

    coin image added

---
+++ CryptoCoin/Services/CoinImageService.swift
@@ -0,0 +1,36 @@
+//
+//  CoinImageService.swift
+//  CryptoCoin
+//
+//  Created by Jiten Patel on 30/01/23.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+
+class CoinImageService{
+    
+    @Published var image: UIImage? = nil
+    
+    private var imageSubscription: AnyCancellable?
+    private let coin: CoinModel
+    
+    init(coin: CoinModel){
+        self.coin = coin
+        getCoinImage()
+    }
+    
+    private func getCoinImage(){
+        guard let url = URL(string: coin.image) else { return }
+        
+        imageSubscription = NetworkingManager.download(url: url)
+            .tryMap({ (data) -> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] returnedImage  in
+                self?.image = returnedImage
+                self?.imageSubscription?.cancel()
+            })
+    }
+}
